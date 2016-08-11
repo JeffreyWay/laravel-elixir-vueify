c28459d54607e9a2d307ef4ff04499ab93615db2
Laracasts <jeffrey@laracasts.com>
2016-08-11 16:47:20 -0400

    Wait for Elixir to be ready

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var Elixir = require('laravel-elixir');
 
-Elixir.config.js.browserify.transformers.push({
-    name: 'vueify',
-
-    // https://github.com/vuejs/vueify#usage
-    options: {}
+Elixir.ready(() => {
+    Elixir.config.js.browserify.transformers.push({
+        name: 'vueify',
+    
+        // https://github.com/vuejs/vueify#usage
+        options: {}
+    });
 });
