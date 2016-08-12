a624e65691910ac0789e311b0727816623115087
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-08-12 10:41:59 -0400

    Revert "Wait for Elixir to be ready"
    This reverts commit c28459d54607e9a2d307ef4ff04499ab93615db2.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var Elixir = require('laravel-elixir');
 
-Elixir.ready(() => {
-    Elixir.config.js.browserify.transformers.push({
-        name: 'vueify',
-    
-        // https://github.com/vuejs/vueify#usage
-        options: {}
-    });
+Elixir.config.js.browserify.transformers.push({
+    name: 'vueify',
+
+    // https://github.com/vuejs/vueify#usage
+    options: {}
 });
